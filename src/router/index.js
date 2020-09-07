6aefe3b19b529ca9558458f98cb9ee740f41ffe7
Luckycarrot42 <eicarriemontecinos@gmail.com>
2020-09-07 19:52:18 -0300

    first commit

--- src/router/index.js
+++ src/router/index.js
@@ -11,12 +11,12 @@ Vue.use(VueRouter)
     component: Home
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '/toys',
+    name: 'Toys',
+    component: () => import(/* webpackChunkName: "Toys" */ '../views/ToysComp.vue'),
+    meta: {
+      login: true //es posible escoger otra palabra para designar necesidad de autentificación
+    }
   }
 ]
 
